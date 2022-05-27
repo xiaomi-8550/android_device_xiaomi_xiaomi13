45d68ef37ab659ac3d0cfaf52d13a6b17fa1c182
Arian <arian.kulmer@web.de>
2022-05-27 16:21:48 +0200

    fuxi: Implement UDFPS
    Squash of the following commits:
    
    fuxi: Drop unnecessary displayfeature from udfps handler
    
      kernel is _smart_
    
    fuxi: Use onAcquired for fod_status
    
    fuxi: Disable fod after unlocking
    
    fuxi: Drop unnecessary udfps extension
    
    fuxi: Rework a bit and enable local hbm via disp_param
    
    fuxi: Handle fingerprint enroll vendor code
    
    fuxi: Notify goodix HAL on fod_press_status changes
    
      Modern fingerprint.goodix_fod6.so has
      moved the polling of this out of the hal and
      replaced it with extCmd 1.
    
    fuxi: Match vendorcode with fuxi fingerprint hal
    
    fuxi: Sswitch UDFPS to IXiaomiFingerprint
    
      add fingerprint init-permissions from
      mfd-daemon.rc to init.target.rc
    
    Co-authored-by: lolipuru <banstellazz@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I4dd5a571a1a48469ce1d25f1f982adef23fc714f

--- device.mk
+++ device.mk
@@ -134,6 +134,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    libudfpshandler
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
@@ -154,6 +162,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc \
     init.mi_thermald.rc \
+    init.mi_udfps.rc \
     init.batterysecret.rc \
     ueventd.xiaomi.rc
 
