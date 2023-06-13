4686cdc734b508af06f92a0aa088ef1fb185e18b
Juhyung Park <qkrwngud825@gmail.com>
2023-06-14 06:08:04 +0900

    fuxi: Set doze_proximity_sensor_supported to false
    Checking the proximity sensor during Doze prevents deep sleep.
    
    Change-Id: I35c3a37966ce40e27fe764d8d6a57a6813a40c8f

--- device.mk
+++ device.mk
@@ -224,6 +224,7 @@ PRODUCT_PACKAGES += \
     FuxiSettings \
     FuxiSettingsProvider \
     FuxiSystemUI \
+    FuxiSystemUIAOSPA \
     FuxiWifiRes \
     FuxiWifiResTarget \
     FuxiWifiResTarget_spf
