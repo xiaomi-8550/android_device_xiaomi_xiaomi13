53999146d540357f21e90323ca4429876453fc1c
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-07-23 19:39:22 +0800

    fuxi: Import device specific overlays from MIUI
    Change-Id: Ic2288c4cdd8b1fd031bf000ea52c81824eb2554d

--- device.mk
+++ device.mk
@@ -224,6 +224,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FuxiFrameworks \
+    FuxiSystemUI \
+    FuxiWifiRes \
+    FuxiWifiResTarget \
+    FuxiWifiResTarget_spf
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor
