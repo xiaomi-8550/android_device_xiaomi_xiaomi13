c4c333d6240c262ea4c1ecdf4f9297e8486acc05
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-03 11:16:45 +0200

    fuxi: Build custom Xiaomi sensors multihal
    Change-Id: I9825c61940e7f18874864dac62a2dc4d7f2944c2

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
