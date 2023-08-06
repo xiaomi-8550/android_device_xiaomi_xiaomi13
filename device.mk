cb7bab6612b34452044f75794a1d09ce7670fd1b
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-06 12:12:10 +0200

    fuxi: Add AAPT configs
    Change-Id: I92499b46e3b3e9d6bcb9d252d2f470ecdc5bd808

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
 
