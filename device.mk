9d154933814e896dac16796605f12bafe2df5548
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    fuxi: Introduce Dolby Atmos
    Converted existing dirac implementation based on observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I2de96ba526284dbbb7eb6c85d1963cf5937e8664

--- device.mk
+++ device.mk
@@ -124,6 +124,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
