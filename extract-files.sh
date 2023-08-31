2bac6745a48218148a4426b893ac8f3eef9d7c53
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-31 23:45:26 +0200

    fuxi: Strip libailab_rawhdr and libxmi_high_dynamic_range_cdsp
     * to big otherwise
    
    Change-Id: I83aa43468220f2c09ad97dde4b3a7a72866964b6

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,12 @@ function blob_fixup() {
         odm/etc/camera/enhance_motiontuning.xml | odm/etc/camera/night_motiontuning.xml | odm/etc/camera/motiontuning.xml)
             sed -i 's/<?xml=/<?xml /g' "${2}"
             ;;
+        vendor/lib64/libailab_rawhdr.so.so)
+            llvm-strip --strip-debug "${2}"
+            ;;
+        vendor/lib64/libxmi_high_dynamic_range_cdsp.so)
+            llvm-strip --strip-debug "${2}"
+            ;;
     esac
 }
 
