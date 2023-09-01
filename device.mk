285e974bd0dd41d89a6ad940f4c3716db3549bdd
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-09-01 17:07:19 +0200

    fix frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk
    Change-Id: Ibd6ef9f23dcec295ffaf41c3209ce182cf707095

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/fuxi/fuxi-vendor.mk)
