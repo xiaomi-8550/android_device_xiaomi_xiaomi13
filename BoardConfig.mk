3be4c9d7017abaf481db1b2d62e5e7ad90948ff3
me-cafebabe <me.cafebabe@gmail.com>
2023-09-10 18:53:52 +0800

    fuxi: Set file permissions for odm/overlayfs/*/*/bin/*
    Change-Id: I8204cfa47bf2e6a378a697b04e829a7f7479c047

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,9 @@ TARGET_SCREEN_DENSITY := 420
 # DTB
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN += $(DEVICE_PATH)/configs/config.fs
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_matrix_xiaomi.xml
