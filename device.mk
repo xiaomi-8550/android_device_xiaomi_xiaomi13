b9b4f9f069e1656287be8d73f588d406df56edee
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-10-30 08:30:49 +0900

    fuxi: Bring back Dolby Atmos
    This reverts commit ea4d2799a7fbb0d2fb39c1a0b1495c967913c6fe, 8f57d7d2380da53f68f39b2494137cb6911e37be, 7d107aa8d6ebbf50d2c254d0ed2a89e90b825a44 and 331a84dbaa4107626b001b988af2484050b8e639.
    
    Change-Id: If9f47ef79103c208de53a511dee7bb79bdd981f6

--- device.mk
+++ device.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-V2-ndk.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama_qssi/audio_policy_configuration.xml
 
@@ -146,6 +145,10 @@ PRODUCT_COPY_FILES += \
 
 # Display
 TARGET_PANEL_DIMENSION_HAS_EXTRA_PRECISION := true
+
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
 
 # DRM
 PRODUCT_PACKAGES += \
