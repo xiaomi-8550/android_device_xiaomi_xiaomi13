4df2e953e2e77c12b3c22438c4e484b04fb9d204
Arian <arian.kulmer@web.de>
2024-02-28 16:53:29 +0100

    xiaomi13: Patch citsensorservice to work with OSS libdisplayconfig.qti
    For some reason xiaomi has duplicated the Init method as ClientImplGet.
    (or it gets automatically generated for them due to some other differences?)
    However, our OSS libdisplayconfig.qti doesn't provide this symbol, but the
    Init method takes the same parameters and seems to be sufficient for
    citsensorservice to work.
    
    Change-Id: I26a88eb379b517f147932310de1da51d65a31c6b

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
         odm/lib64/hw/displayfeature.default.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        odm/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so | odm/overlayfs/nuwa/odm/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
+            sed -i 's/_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE/_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0/g' "${2}"
+            ;;
         odm/lib64/libailab_rawhdr.so | odm/lib64/libxmi_high_dynamic_range_cdsp.so | odm/overlayfs/nuwa/odm/lib64/libailab_rawhdr.so | odm/overlayfs/nuwa/odm/lib64/libxmi_high_dynamic_range_cdsp.so)
             "${ANDROID_ROOT}"/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip --strip-debug "${2}"
             ;;
