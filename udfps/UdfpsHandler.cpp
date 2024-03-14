d01867da9a40d3abe3595b8e49dd5d6338ff1c1f
Arian <arian.kulmer@web.de>
2024-03-14 14:38:35 +0100

    xiaomi13: Change verbose logging to DEBUG
    Change-Id: Ic3c4401e61b54445f9acc0d1e53c75c21b6a6ee6

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -204,7 +204,7 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
     }
 
     void onFingerDown(uint32_t x, uint32_t y, float /*minor*/, float /*major*/) {
-        LOG(INFO) << __func__ << "x: " << x << ", y: " << y;
+        LOG(DEBUG) << __func__ << "x: " << x << ", y: " << y;
         // Track x and y coordinates
         lastPressX = x;
         lastPressY = y;
@@ -214,13 +214,13 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
     }
 
     void onFingerUp() {
-        LOG(INFO) << __func__;
+        LOG(DEBUG) << __func__;
         // Ensure touchscreen is aware of the press state, ideally this is not needed
         setFingerDown(false);
     }
 
     void onAcquired(int32_t result, int32_t vendorCode) {
-        LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
+        LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
         if (result == FINGERPRINT_ACQUIRED_GOOD) {
             // Set finger as up to disable HBM already, even if the finger is still pressed
             setFingerDown(false);
@@ -236,7 +236,7 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
     }
 
     void cancel() {
-        LOG(INFO) << __func__;
+        LOG(DEBUG) << __func__;
         setFodStatus(FOD_STATUS_OFF);
     }
 
